25a61405bb192f4c2934894fc88977b8279cbfad
Fred Awumey <fredawumeyfafa@gmail.com>
2022-11-20 16:21:29 +0000

    Update for footer view

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+
+  var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
+
+  // swiper
+ 
+      // var swiper = new Swiper(".mySwiper", {
+      //   slidesPerView: 2,
+      //   spaceBetween: 30,
+      //   slidesPerGroup: 2,
+      //   loop: true,
+      //   loopFillGroupWithBlank: true,
+      //   pagination: {
+      //     el: ".swiper-pagination",
+      //     clickable: true,
+      //   },
+      //   navigation: {
+      //     nextEl: ".swiper-button-next",
+      //     prevEl: ".swiper-button-prev",
+      //   },
+      // });
